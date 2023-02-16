16f00451e506dac0eddf8497e0983ea17b87bccc
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-02-16 13:42:11 +0600

    Bug fixed

--- JS/indexdev.js
+++ JS/indexdev.js
@@ -12,29 +12,75 @@ const Balance=document.getElementById('balanceid');
 
 let Remain;
 
+function GetStringValue(inputBox){
+    const GetString=inputBox.value;
+    return GetString;
+}
+
+function GetNumberFromString(stringValue){
+    const Number=parseFloat(stringValue);
+    if(Number<0){
+        return "Give Positive Number"
+    }else{
+        return Number;
+    }
+
+}
+
 CalculateButton.addEventListener('click',function(){
 
-    const IncomeString=IncomeInput.value;
-    const FoodString=FoodInput.value;
-    const RentString=RentInput.value;
-    const ClothString=ClothInput.value;
+    const IncomeString=GetStringValue(IncomeInput);
+    const FoodString=GetStringValue(FoodInput);
+    const RentString=GetStringValue(RentInput);
+    const ClothString=GetStringValue(ClothInput);
 
-    const IncomeAmount=parseFloat(IncomeString);
-    const FoodAmount=parseFloat(FoodString);
-    const RentAmount=parseFloat(RentString);
-    const ClothAmount=parseFloat(ClothString);
+    const IncomeAmount=GetNumberFromString(IncomeString);
+    const FoodAmount=GetNumberFromString(FoodString);
+    const RentAmount=GetNumberFromString(RentString);
+    const ClothAmount=GetNumberFromString(ClothString);
 
     if(IncomeString===''){
         alert("Income Input is empty");
     }else{
-        if(isNaN(IncomeAmount) || isNaN(FoodAmount) || isNaN(RentAmount) || isNaN(ClothAmount)){
-           alert("Please Give Number");
-        }else{
-          const Total=FoodAmount+RentAmount+ClothAmount;
-          TotalExpenses.innerText=Total;
-          Remain=IncomeAmount-Total;
-          Balance.innerText=Remain;
-        }
+       if(isNaN(IncomeAmount)){
+         alert(IncomeAmount)
+       }else{
+         if(FoodString===''){
+            alert("তোমার কি খাওয়া দাওয়া লাগবে না? হাওইয়া খাইয়া বাঁচবা");
+         }else{
+            if(isNaN(FoodAmount)){
+                alert(FoodAmount);
+            }else{
+                if(RentString===''){
+                    alert('নিজের বাসা পাইছো? ভাড়া দেওয়া না?');
+                }else{
+                    if(isNaN(RentAmount)){
+                        alert(RentAmount);
+                    }else{
+                        if(ClothString===''){
+                            alert('লেংটা থাকবা?');
+                        }else{
+                            if(isNaN(ClothAmount)){
+                                alert(ClothAmount);
+                            }else{
+                                const Total=FoodAmount+RentAmount+ClothAmount;
+
+                                if(Total>IncomeAmount){
+                                    alert('কাট ইউর কোট একোরডিং ইয়োর ক্লোথ');
+                                }else{
+                                    TotalExpenses.innerText=Total;
+
+                                    Remain=IncomeAmount-Total;
+                                    Balance.innerHTML=Remain;
+                                }
+                              
+                            }
+                        }
+                    }
+                }
+            }
+         }
+       }
     }
 
    
@@ -47,16 +93,54 @@ const SavingAmount=document.getElementById('savingamountid');
 const RemainingBalance=document.getElementById('remainbalanceid');
 
 SaveButton.addEventListener('click',function(){
-    const SaveString=SaveInputBox.value;
-    const SaveAmount=parseFloat(SaveString);
+    const SaveString=GetStringValue(SaveInputBox);
+    const SaveAmount=GetNumberFromString(SaveString);
 
-    if(isNaN(SaveAmount)){
-        alert("Give Number");
+    const IncomeString=GetStringValue(IncomeInput);
+    const IncomeAmount=GetNumberFromString(IncomeString);
+
+    if(IncomeString===''){
+        alert('Income দেও নাই, Save করতে আসছো?')
     }else{
-        const LastAmount=(parseFloat(IncomeInput.value)*SaveAmount)/100;
-        SavingAmount.innerText=LastAmount;
+        if(isNaN(IncomeAmount)){
+            alert('Income Minus হয়? গাঁজা খাইছোস?');
+        }else{
+            if(SaveString===''){
+                alert('কত save করতে চাস দিবি না?');
+            }else{
+                if(isNaN(SaveAmount)){
+                    alert(SaveAmount);
+                }else{
+                    const ExpensesBalanceString=TotalExpenses.innerText;
+                    const TotalBalanceString=Balance.innerText;
+                    if(ExpensesBalanceString==='' || TotalBalanceString===''){
+                        alert('আগে সব Calculate করো');
+                    }else{
 
-        const Remaing_Balace=Remain-LastAmount;
-        RemainingBalance.innerText=Remaing_Balace;
+                      
+                        const LastAmount=(parseFloat(IncomeInput.value)*SaveAmount)/100;
+                        if(parseFloat(TotalBalanceString)<LastAmount){
+                            alert('এতো কিভাবে সেভ করবি? ব্যালেন্স আছে?');
+                        }else{
+                            SavingAmount.innerText=LastAmount;
+   
+                            const Remaing_Balace=Remain-LastAmount;
+                            RemainingBalance.innerText=Remaing_Balace;
+                        }
+                       
+                    }
+                }
+            }
+        }
     }
+
+    // if(isNaN(SaveAmount)){
+    //     alert("Give Number");
+    // }else{
+    //     const LastAmount=(parseFloat(IncomeInput.value)*SaveAmount)/100;
+    //     SavingAmount.innerText=LastAmount;
+
+    //     const Remaing_Balace=Remain-LastAmount;
+    //     RemainingBalance.innerText=Remaing_Balace;
+    // }
 })
